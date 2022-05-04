292de068ebafcb8aaa57fe54186b486f806ca76b
sahilyeole <sahilyeole93@gmail.com>
2022-05-04 13:54:55 +0530

    /

---
+++ js/mdn/4. Classes in js/4.js
@@ -0,0 +1,11 @@
+class Person{
+    name;
+    constructor(name){
+        this.name =name;
+    }
+    introduceSelf(){
+        console.log(`Hi! Im ${this.name}`);
+    }
+}
+// read the article its not too long
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Classes_in_JavaScript
