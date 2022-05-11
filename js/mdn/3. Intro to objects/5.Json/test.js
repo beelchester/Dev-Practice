ada1139a4e0496727154c288df71b1eced7c6cfb
sahilyeole <sahilyeole93@gmail.com>
2022-05-11 21:42:13 +0530

    /

---
+++ js/mdn/3. Intro to objects/5.Json/test.js
@@ -0,0 +1,30 @@
+// ! Incomplete
+
+// const section = document.querySelector('section');
+
+//     let para1 = document.createElement('p');
+//     let para2 = document.createElement('p');
+//     let motherInfo = 'The mother cats are called ';
+//     let kittenInfo;
+//     const requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/tasks/json/sample.json';
+
+//     fetch(requestURL)
+//     .then(response => response.text())
+//     .then(text => displayCatInfo(text))
+//     // .then(cats => JSON.parse(text))
+//     function displayCatInfo(catString) {
+//       let total = 0;
+//       let male = 0;
+
+//       // Add your code here
+//       let cats = JSON.parse(catString);
+
+//       const name1 = cats[1].name;
+//       // Don't edit the code below here!
+
+//       para1.textContent = motherInfo;
+//       para2.textContent = kittenInfo;
+//     }
+
+//     section.appendChild(para1);
+//     section.appendChild(para2);
