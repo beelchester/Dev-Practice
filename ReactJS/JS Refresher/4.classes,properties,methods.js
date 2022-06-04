68e54fbe35208fc56b9b98a9990b828e0671502c
sahilyeole <sahilyeole93@gmail.com>
2022-06-04 22:10:38 +0530

    /

--- ReactJS/JS Refresher/4.classes,properties,methods.js
+++ ReactJS/JS Refresher/4.classes,properties,methods.js
@@ -2,10 +2,4 @@
 // * this is ES7 syntax... next gen js
 class Person{
   name="Mark"
-  // ! new syntax for method too
-  // * we use arrow function and using this is better bcz its solve some this. problems
-  method =() =>{console.log(this.name)}
-}
-Person.method()
-// ? use jsbin for running this code and choose ES6/Babel
 
