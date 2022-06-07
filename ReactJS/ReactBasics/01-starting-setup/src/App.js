a60bcbeb1d2c819419dd66883939fb3a5f1cdc82
sahilyeole <sahilyeole93@gmail.com>
2022-06-07 22:55:06 +0530

    /

--- ReactJS/ReactBasics/01-starting-setup/src/App.js
+++ ReactJS/ReactBasics/01-starting-setup/src/App.js
@@ -1,9 +1,10 @@
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <h2>Let's get started</h2>
     </div>
   );
 }
+// ? This is a JSX syntax invented by react and it works because of the dependancy we've already added
 
 export default App;
