90ffde9cf65caf624d404bfd2a4af2aec43c9a8c
sahilyeole <sahilyeole93@gmail.com>
2022-06-11 11:18:02 +0530

    /

--- ReactJS/2.ReactExpenseTracker/01-starting-setup/src/App.js
+++ ReactJS/2.ReactExpenseTracker/01-starting-setup/src/App.js
@@ -34,3 +34,4 @@ function App() {
 
 export default App;
 
+// ! Go to ExpenseItems 
--- ReactJS/2.ReactExpenseTracker/01-starting-setup/src/components/Expense/ExpenseItems.js
+++ ReactJS/2.ReactExpenseTracker/01-starting-setup/src/components/Expense/ExpenseItems.js
@@ -1,12 +1,15 @@
-
+import React from 'react'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card'
 import './ExpenseItems.css';
 
 function ExpenseItems(props) {
   
+  function clickHandler(){
+   console.log("clicked")
+  //  the state of the program wont be updated once rendered so to update state we use useState() function from react library
+  }
   return (
-   
    
       <Card className='expense-item'>
         <ExpenseDate date={props.date}/> 
@@ -15,6 +18,9 @@ function ExpenseItems(props) {
           <h2>{props.title}</h2>
         </div>
         <div className='expense-item__price'>{props.amount}</div>
+        <button onClick={clickHandler}>click</button> 
+        {/* dont use () after mentioning a function here bcz it will just be pointing not calling it and we 
+        dont want it to call as we want to run it only when button is clicked*/}
       </Card>
 
   );
