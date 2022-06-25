312a54ef084da9df945511baa0c07c84e6b9da43
sahilyeole <sahilyeole93@gmail.com>
2022-06-25 13:07:24 +0530

    /

--- ReactJS/2.ReactExpenseTracker/01-starting-setup/src/App.js
+++ ReactJS/2.ReactExpenseTracker/01-starting-setup/src/App.js
@@ -1,48 +1,33 @@
-import React,{useState} from "react";
-import ExpenseData from "./components/Expense/ExpenseData";
-import NewExpense from './components/NewExpense/NewExpense'
-const DeafultExpenses = [
-  {
-    id: 'e1',
-    title: 'Toilet Paper',
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-  {
-    id: 'e3',
-    title: 'Car Insurance',
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: 'e4',
-    title: 'New Desk (Wooden)',
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  }
-];
+import React from 'react';
+import Expenses from './components/Expenses'
 function App() {
-  //* to add user input to our data
-const [expenses,setExpenses] = useState(DeafultExpenses)
-console.log(DeafultExpenses[0].date.getFullYear())
-const addExpenseHandler = (expense) => {
-  setExpenses((prevExpenses)=>{
-    return [expense,...prevExpenses]
-  })  
-
-}
+  const defaultExpenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
   return (
-
     <div>
-     <NewExpense onAddExpense={addExpenseHandler} />
-  
-      <ExpenseData items={expenses}/>
+      
+      <Expenses item={defaultExpenses}/>
     </div>
   );
 }
 
-
 export default App;
-
-
