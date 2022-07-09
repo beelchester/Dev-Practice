108a441edc0f3cd94932f7051925a12d211b1eb2
sahilyeole <sahilyeole93@gmail.com>
2022-07-09 11:48:59 +0530

    /

--- ReactJS/2.ReactExpenseTracker/01-starting-setup/src/App.js
+++ ReactJS/2.ReactExpenseTracker/01-starting-setup/src/App.js
@@ -1,4 +1,5 @@
-import ExpenseData from "./components/ExpenseData";
+import ExpenseData from "./components/Expense/ExpenseData";
+import NewExpense from './components/NewExpense/NewExpense'
 function App() {
   const expenses = [
     {
@@ -21,47 +22,21 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+const addExpenseHandler = (expense) => {
+  console.log("inside App.js")
+  console.log(expense)
+}
   return (
 
     <div>
       <h2>Expense Tracker</h2>
-      {/* after ExpenseItems notes start from here */}
-      {/* <ExpenseItems></ExpenseItems> can also be created as below (self closing tag)*/}
-      {/* Here we are adding custom js by using {} in custom attributes which will get passed to ExpenseItems */}
-      {/* <ExpenseItems title={expenses[0].title} amount = {expenses[0].amount} date = {expenses[0].date}/>
-      <ExpenseItems title={expenses[1].title} amount = {expenses[1].amount} date = {expenses[1].date}/>
-      <ExpenseItems title={expenses[2].title} amount = {expenses[2].amount} date = {expenses[2].date}/>
-      <ExpenseItems title={expenses[3].title} amount = {expenses[3].amount} date = {expenses[3].date}/> */}
-      {/* now go back to ExpenseItems.js */}
-      {/* now adding ExpenseItems in ExpenseData.js instead of App.js*/}
+     <NewExpense onAddExpense={addExpenseHandler} />
       <ExpenseData expenses={expenses}/>
     </div>
   );
 }
-// ? This is a JSX syntax invented by react and it works because of the dependancy we've already added
+
 
 export default App;
 
-// * jsx is js xml and html is also xml
-// * jsx syntax is not recognized by the browser but instead it is transfordmed to normal js code which can be complex for dev
-
-// * with react we build our own custom html elements i.e components
-// * components are basically custom html elements
-// ? we have Declarative approach in react
-// with React
-// we defined the desired target state and React is then responsible
-// for generating and running the actual DOM instructions
-// ? DOM manipulation in js is imperative approach
-// * It is a good practice to create new files for new components
-// So now we are creating a new folder in src called components 
-// * we will not add App.js in that because it is a special type of component i.e root component
-// * It is the main component that will be getting rendered in the index.js file
-
-//* in react we build a component tree that is nested components, components inside components
-// * and the the top of that App.js lies 
-// ? Its a convention to write component file names in all first letter of words in capital
-// Ex: ExpenseItems.js
-//! go to ExpenseItems.js for furthur notes
-
-// ! Composition: process of building UI using small blocks i.e components
-// !Go to Card.js
+// ! Go to ExpenseItems 
