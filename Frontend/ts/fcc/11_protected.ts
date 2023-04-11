c9beefe155b69f9b72e150013ba1541b0d57cf9c
Sahil Yeole <sahilyeole93@gmail.com>
2023-04-11 23:02:34 +0530

    /

---
+++ Frontend/ts/fcc/11_protected.ts
@@ -0,0 +1,13 @@
+class Base {
+  protected m = 10;
+}
+class Derived extends Base {
+  // No modifier, so default is 'public'
+  m = 15;
+}
+const d = new Derived();
+console.log(d.m); // OK
+
+// protected members are only visible to subclasses of the class they’re declared in.
+
+
