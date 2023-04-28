3fd81b6825679c8371bde784e07f16cd8df62eb0
Sahil Yeole <sahilyeole93@gmail.com>
2023-04-28 09:02:24 +0530

    /

--- Rust/the_rust_programming_language/ch3/1_variables_mutability/var_mut/src/main.rs
+++ Rust/the_rust_programming_language/ch3/1_variables_mutability/var_mut/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    // let x = 5;
+    // println!("The value of x is: {x}");
+    // x = 6; // this will throw an error because x is immutable
+    // println!("The value of x is: {x}");
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}"); // this will work because x is mutable
 }
+
+/*
+ again, by default variables in Rust are immutable, this provides safety and concurrency
+concurrency means that multiple parts of your program can execute independently from each other at the same time
+
+ */
