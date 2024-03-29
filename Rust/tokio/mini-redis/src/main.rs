35d340e6100653af0038b58bf91664cbe63246a0
Sahil Yeole <sahilyeole93@gmail.com>
2024-03-29 23:28:32 +0530

    add tokio

---
+++ Rust/tokio/mini-redis/src/main.rs
@@ -0,0 +1,57 @@
+use mini_redis::{Connection, Frame};
+use tokio::net::{TcpListener, TcpStream};
+
+#[tokio::main]
+async fn main() {
+    // Bind the listener to the address
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    loop {
+        // The second item contains the IP and port of the new connection.
+        let (socket, _) = listener.accept().await.unwrap();
+        // A new task is spawned for each inbound socket. The socket is
+        // moved to the new task and processed there.
+        tokio::spawn(async move {
+            process(socket).await;
+        });
+    }
+}
+
+async fn process(socket: TcpStream) {
+    use mini_redis::Command::{self, Get, Set};
+    use std::collections::HashMap;
+
+    // A hashmap is used to store data
+    let mut db = HashMap::new();
+
+    // Connection, provided by `mini-redis`, handles parsing frames from
+    // the socket instead of byte streams.
+    let mut connection = Connection::new(socket);
+
+    // Use `read_frame` to receive a command from the connection.
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                // The value is stored as `Vec<u8>`
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                println!("db: {:?}", db);
+                Frame::Simple("OK".to_string()) // simple means a status message
+            }
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    // `Frame::Bulk` expects data to be of type `Bytes`. This
+                    // type will be covered later in the tutorial. For now,
+                    // `&Vec<u8>` is converted to `Bytes` using `into()`.
+                    println!("db in Get: {:?}", db);
+                    Frame::Bulk(value.clone().into()) // bulk means a single binary data
+                } else {
+                    Frame::Null // null means no data
+                }
+            }
+            cmd => panic!("unimplemented {:?}", cmd),
+        };
+
+        // Write the response to the client
+        connection.write_frame(&response).await.unwrap();
+    }
+}
---
+++ Rust/tokio/spawning/src/main.rs
@@ -0,0 +1,59 @@
+use std::sync::{Arc, Mutex};
+#[tokio::main]
+async fn main() {
+    // this will be never executed unless awaited
+    let task = tokio::spawn(async {
+        println!("Hello from a task!");
+    });
+
+    task.await.unwrap();
+
+    let mut num = 1;
+    let moved_task = tokio::spawn(async move {
+        num += 1;
+        println!("Number is: {}", num);
+    });
+    println!("Num in main is: {}", num);
+    moved_task.await.unwrap();
+    println!("Num again in main is: {}", num);
+    // still 1 because it was moved to the task and not shared
+
+    let arc_num = Arc::new(10);
+
+    let arc_task = tokio::spawn(async move {
+        println!("Number is: {}", arc_num);
+        let _n = arc_num.clone();
+        // n += 1; // cant do this because Arc is not mutable
+    });
+    arc_task.await.unwrap();
+
+    let mutex_num = Arc::new(Mutex::new(10)); // Mutex is a smart pointer that allows multiple threads to access the same data
+
+    let mutex_task = tokio::spawn(async move {
+        let mut num = mutex_num.lock().unwrap();
+        *num += 1;
+        println!("Number is: {}", *num);
+    });
+
+    mutex_task.await.unwrap();
+    // println!("Num again in main is: {}", mutex_num.lock().await);
+    // error because shared state having mutex is not allowed to be accessed by multiple tasks
+    // as mutex is exclusive access and we are creating two references to the same data
+    // so we need to use clone to create a new reference to the data and use that same reference in both tasks
+
+    let new_mutex_num = Arc::new(Mutex::new(100));
+    let mutex_num_clone = Arc::clone(&new_mutex_num);
+    // now lets move the clone to the task
+    let mutex_task_clone = tokio::spawn(async move {
+        let mut num = mutex_num_clone.lock().unwrap();
+        *num += 1;
+        println!("Number is: {}", *num);
+    });
+
+    mutex_task_clone.await.unwrap();
+    // accessing same clone referenced data in main
+    println!("mutex num in main is: {}", new_mutex_num.lock().unwrap());
+
+    let mut_plus_one = new_mutex_num.lock().unwrap();
+    println!("mutex num +1 in main is: {}", *mut_plus_one + 1);
+}
